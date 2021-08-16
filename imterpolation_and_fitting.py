e7241efef87e155504867fe9a2cd47b9d47934da
ZhengHaoWen <705680835@qq.com>
2021-08-16 14:31:32 +0800

    refactor:Use draw lines instead of draw circles for interpolation and fitting; style: modify error file name: imterpolation_and_fitting.py ->interpolation_and_fitting.py

--- imterpolation_and_fitting.py
+++ imterpolation_and_fitting.py
@@ -1,5 +1,6 @@
 import taichi as ti
 import math
+import numpy as np
 
 width = 640
 height = 320
@@ -8,8 +9,8 @@ gui = ti.GUI('Window Title', (width, height))
 NUM_MAX_POINT = 16
 
 x = ti.field(ti.i32)
-pixels = ti.field(dtype=float, shape=(width, height))
 
+points = ti.Vector.field(2, dtype=ti.i32, shape=(width))
 ti.root.dense(ti.i, 2).dynamic(ti.j, NUM_MAX_POINT).place(x)
 
 
@@ -23,7 +24,8 @@ def add_point(px: ti.i32, py: ti.i32):
 def pixel_interpolation():
     for i in range(0, width + 1):
         real = lagrange_interpolation(i)
-        pixels[i, real] = 1
+        points[i][0] = i
+        points[i][1] = real
 
 
 @ti.func
@@ -40,13 +42,21 @@ def lagrange_interpolation(i):
     return summ
 
 
+points_numpy = np.zeros((width, 2))
+click_points = []
 while gui.running:
     if gui.get_event(ti.GUI.PRESS) and gui.event.key == "LMB":
-        pixels.fill(0)
+
         cur_x, cur_y = gui.get_cursor_pos()
         cur_x = math.ceil(cur_x * width)
         cur_y = math.ceil(cur_y * height)
+        click_points.append([cur_x,cur_y])
         add_point(cur_x, cur_y)
         pixel_interpolation()
-    gui.set_image(pixels)
+        points_numpy = points.to_numpy()
+
+    for cp in click_points:
+        print(cp)
+        gui.circle(np.array(cp)/[width, height],radius=3)
+    gui.lines(points_numpy[0:width - 2, :] / [width, height], points_numpy[1:width - 1, :] / [width, height], radius=1)
     gui.show()
