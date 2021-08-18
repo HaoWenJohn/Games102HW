ec8bb1728cc777ae50714080ad794347ef9b1801
ZhengHaoWen <705680835@qq.com>
2021-08-18 10:25:24 +0800

    refactor:use data-oriented style

--- imterpolation_and_fitting.py
+++ imterpolation_and_fitting.py
@@ -8,55 +8,63 @@ gui = ti.GUI('Window Title', (width, height))
 
 NUM_MAX_POINT = 16
 
-x = ti.field(ti.i32)
 
-points = ti.Vector.field(2, dtype=ti.i32, shape=(width))
-ti.root.dense(ti.i, 2).dynamic(ti.j, NUM_MAX_POINT).place(x)
+class DrawLine:
+    def __init__(self, window_width):
+        self.points = ti.field(ti.i32)
+        self.width = window_width
+        self.results = ti.Vector.field(2, dtype=ti.i32, shape=self.width)
+        ti.root.dense(ti.i, 2).dynamic(ti.j, NUM_MAX_POINT).place(self.points)
 
+    @ti.kernel
+    def add_point(self, x: ti.i32, y: ti.i32):
+        ti.append(self.points.parent(), 0, x)
+        ti.append(self.points.parent(), 1, y)
 
-@ti.kernel
-def add_point(px: ti.i32, py: ti.i32):
-    ti.append(x.parent(), 0, px)
-    ti.append(x.parent(), 1, py)
-
-
-@ti.kernel
-def pixel_interpolation():
-    for i in range(0, width + 1):
-        real = lagrange_interpolation(i)
-        points[i][0] = i
-        points[i][1] = real
+    def pixel_interpolation(self):
+        pass
 
 
-@ti.func
-def lagrange_interpolation(i):
-    summ = 0.0
-    for outer in range(0, ti.length(x.parent(), 1)):
-        multi = 1.0
-        for inner in range(0, ti.length(x.parent(), 1)):
-            if outer == inner:
-                continue
-            multi = multi * (i - x[0, inner]) / (x[0, outer] - x[0, inner])
-        summ = summ + multi * x[1, outer]
+@ti.data_oriented
+class LagrangeInterpolation(DrawLine):
+    def __init__(self, window_width):
+        super().__init__(window_width)
 
-    return summ
+    @ti.func
+    def lagrange_interpolation(self, i):
+        summ = 0.0
+        for outer in range(0, ti.length(self.points.parent(), 1)):
+            multi = 1.0
+            for inner in range(0, ti.length(self.points.parent(), 1)):
+                if outer == inner:
+                    continue
+                multi = multi * (i - self.points[0, inner]) / (self.points[0, outer] - self.points[0, inner])
+            summ = summ + multi * self.points[1, outer]
+
+        return summ
+
+    @ti.kernel
+    def pixel_interpolation(self):
+        for i in range(0, self.width + 1):
+            real = self.lagrange_interpolation(i)
+            self.results[i][0] = i
+            self.results[i][1] = real
 
 
-points_numpy = np.zeros((width, 2))
-click_points = []
+@ti.data_oriented
+class GuassInterpolation(DrawLine):
+    def __init__(self, window_width):
+        super().__init__(window_width)
+
+
+drawLine: DrawLine = LagrangeInterpolation(width)
+res = np.zeros((width, 2))
 while gui.running:
     if gui.get_event(ti.GUI.PRESS) and gui.event.key == "LMB":
-
         cur_x, cur_y = gui.get_cursor_pos()
-        cur_x = math.ceil(cur_x * width)
-        cur_y = math.ceil(cur_y * height)
-        click_points.append([cur_x,cur_y])
-        add_point(cur_x, cur_y)
-        pixel_interpolation()
-        points_numpy = points.to_numpy()
+        drawLine.add_point(math.ceil(cur_x * width), math.ceil(height * cur_y))
+        drawLine.pixel_interpolation()
+        res = drawLine.results.to_numpy()
+    gui.lines(res[0:width - 2, :] / [width, height], res[1:width - 1, :] / [width, height], radius=1)
 
-    for cp in click_points:
-        print(cp)
-        gui.circle(np.array(cp)/[width, height],radius=3)
-    gui.lines(points_numpy[0:width - 2, :] / [width, height], points_numpy[1:width - 1, :] / [width, height], radius=1)
     gui.show()
